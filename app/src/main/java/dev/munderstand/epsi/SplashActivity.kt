ebd2d6266dc545d98924bf57f729308578a3319a
Mundersatnd <munderstand101@gmail.com>
2023-03-08 18:55:53 +0100

    Added SplashActivity (DayNight)

---
+++ app/src/main/java/dev/munderstand/epsi/SplashActivity.kt
@@ -0,0 +1,30 @@
+package dev.munderstand.epsi
+
+import android.content.Intent
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val newIntent= Intent(application,HomeActivity::class.java)
+            startActivity(newIntent)
+            finish()
+        },2000)
+
+        // Change the logo for dark mode
+        val logo = findViewById<ImageView>(R.id.splash_logo)
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            logo.setImageResource(R.drawable.dummy_logo_night)
+        }
+
+    }
+}
